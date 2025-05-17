14ca08383f40ce2fdaf5aaeaba397cff60ff3d90
andylamax <andylamax@programmer.net>
2025-05-17 22:03:53 +0300

    finalized missing parts

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ subprojects {
     version = v
 
     configure<MavenPublishBaseExtension> {
-        publishToMavenCentral(SonatypeHost.DEFAULT, automaticRelease = true)
+        publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL,automaticRelease = true)
 
         signAllPublications()
 
